1073dd61076a5c704795e5f454ea84d870bf1e4b
kriperi <mikkelfs@gmail.com>
2021-09-06 16:01:41 +0200

    test

---
+++ src/main/java/no/kristiania/http/HttpClient.java
@@ -0,0 +1,11 @@
+package no.kristiania.http;
+
+public class HttpClient {
+
+    public HttpClient(String s, int i, String s1) {
+    }
+
+    public int getStatusCode(){
+        return 0;
+    }
+}
---
+++ src/test/java/no/kristiania/http/HttpClientTest.java
@@ -0,0 +1,15 @@
+package no.kristiania.http;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HttpClientTest {
+    @Test
+    void shouldReciveStatus() {
+        HttpClient client = new HttpClient("httpbin.org", 80, "/status/200");
+        assertEquals(200, client.getStatusCode());
+    }
+
+
+}
